2991d12b2e92d171959fbe6c69ec188e04240275
Alex Day <ahday4@gmail.com>
2017-03-04 14:49:35 -0500

    Updated script to include variables and package installation

--- greenfield-ansible.sh
+++ greenfield-ansible.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
 #Declare variables
+USER_ID="john"
+PKG_LIST="openssh-server python ansible"
+SSH_KEY_TYPE=rsa
+SSH_KEY_LENGTH=4096
 
 #Declare functions
 
-#TODO Install and configure OpenSSH-Server & SSH
+#Install packages
+apt-get update
 
-#TODO Install and configure Ansible & Dependencies
+apt-get install -y $PKG_LIST
+
+apt-get upgrade
+
+#Configure SSH
+cd ~
+ssh-keygen -t $SSH_KEY_TYPE -b $SSH_KEY_LENGTH
+
+#Configre Ansible
